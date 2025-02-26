f0c14f3f81af46b6da7a3ee120be8a27c3600673
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-25 22:05:55 -0700

    login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,45 +1,85 @@
-const bcrypt = require('bcryptjs');
-const jdenticon = require('jdenticon');
-const path = require('node:path');
-const fs = require('node:fs');
-const { prisma } = require('../prisma/prismaClient');
+const bcrypt = require("bcryptjs");
+const jdenticon = require("jdenticon");
+const path = require("node:path");
+const fs = require("node:fs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const { prisma } = require("../prisma/prismaClient");
 
 const UserController = {
   register: async (req, res) => {
     const { email, name, password } = req.body;
 
     if (!email || !name || !password) {
-      return res.status(400).json({ error: 'All fields required!' })
+      return res.status(400).json({ error: "All fields required!" });
     }
 
     try {
-      const existingUser = await prisma.user.findUnique(({ where: { email } }));
+      const existingUser = await prisma.user.findUnique({ where: { email } });
 
       if (existingUser) {
-        return res.status(400).json({ error: 'User already registered!' })
+        return res.status(400).json({ error: "User already registered!" });
       }
 
       const hashedPassword = await bcrypt.hash(password, 10);
 
       const png = jdenticon.toPng(name, 200);
       const avatarName = `${name}_${Date.now()}.png`;
-      const avatarPath = path.join(__dirname, '../uploads', avatarName);
+      const avatarPath = path.join(__dirname, "../uploads", avatarName);
       fs.writeFileSync(avatarPath, png);
 
       const user = await prisma.user.create({
-        data: { email, password: hashedPassword, name, avatarUrl: `/uploads/${avatarPath}` }
-      })
+        data: {
+          email,
+          password: hashedPassword,
+          name,
+          avatarUrl: `/uploads/${avatarPath}`,
+        },
+      });
 
       res.json(user);
     } catch (error) {
-      console.error('Error', error.message);
-      res.status(500).json({ error: 'Internal server error' });
+      console.error("Error", error.message);
+      res.status(500).json({ error: "Internal server error" });
     }
   },
-  login: async (req, res) => { res.send('login') },
-  getUserById: async (req, res) => { res.send('getUserById') },
-  updateUser: async (req, res) => { res.send('updateUser') },
-  current: async (req, res) => { res.send('currentUser') },
-}
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ error: "All fields required!" });
+    }
+
+    try {
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) {
+        return res.status(400).json({ error: "Wrong email or password" });
+      }
+
+      const valid = await bcrypt.compare(password, user.password);
+
+      if (!valid) {
+        return res.status(400).json({ error: "Wrong email or password" });
+      }
+
+      const token = jwt.sign({ userId: user.id }, process.env.SECRET_KEY);
+
+      res.json({ token });
+    } catch (error) {
+      console.error("Login error", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  },
+  getUserById: async (req, res) => {
+    res.send("getUserById");
+  },
+  updateUser: async (req, res) => {
+    res.send("updateUser");
+  },
+  current: async (req, res) => {
+    res.send("currentUser");
+  },
+};
 
 module.exports = UserController;
