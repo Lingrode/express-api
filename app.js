53b337905c7e4983fed3d194c425ae40e63d7c00
Illia Hrymalo <grimaloilya@gmail.com>
2025-02-25 22:13:14 -0700

    middleware token auth

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const fs = require('node:fs')
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const fs = require("node:fs");
 
 const app = express();
 
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.set('view engine', 'jade');
+app.set("view engine", "jade");
 
-app.use('/uploads', express.static('uploads'))
+app.use("/uploads", express.static("uploads"));
 
-app.use('/api', require('./routes'))
+app.use("/api", require("./routes"));
 
-if (!fs.existsSync('uploads')) {
-  fs.mkdirSync('uploads')
+if (!fs.existsSync("uploads")) {
+  fs.mkdirSync("uploads");
 }
 
 // catch 404 and forward to error handler
@@ -31,11 +30,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,23 @@
-const express = require('express')
-const multer = require('multer');
-const { UserController } = require('../controllers');
+const express = require("express");
+const multer = require("multer");
+const { UserController } = require("../controllers");
+const authenticateToken = require("../middleware/auth");
 
 const router = express.Router();
 
-const uploadDestination = 'uploads'
+const uploadDestination = "uploads";
 
 const storage = multer.diskStorage({
   destination: uploadDestination,
-  filename: (req, file, callback) => callback(null, file.originalname)
-})
+  filename: (req, file, callback) => callback(null, file.originalname),
+});
 
-const uploads = multer({ storage: storage })
+const uploads = multer({ storage: storage });
 
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
-router.get('/current', UserController.current)
-router.get('/users/:id', UserController.getUserById)
-router.put('/users/:id', UserController.updateUser)
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.get("/current", authenticateToken, UserController.current);
+router.get("/users/:id", authenticateToken, UserController.getUserById);
+router.put("/users/:id", authenticateToken, UserController.updateUser);
 
-module.exports = router
+module.exports = router;
