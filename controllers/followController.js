80bc4ffe6788e244d25ac33a9e95169230a31b21
Illia Hrymalo <grimaloilya@gmail.com>
2025-03-02 22:17:10 -0700

    unfollow user controller

--- controllers/followController.js
+++ controllers/followController.js
@@ -32,7 +32,29 @@ const FollowController = {
     }
   },
   unfollowUser: async (req, res) => {
-    res.send("unfollowUser");
+    const { followingId } = req.body;
+    const userId = req.user.userId;
+
+    try {
+      const follows = await prisma.follows.findFirst({
+        where: { AND: [{ followerId: userId }, { followingId }] },
+      });
+
+      if (!follows) {
+        return res
+          .status(404)
+          .json({ error: "You are not followed to this user" });
+      }
+
+      await prisma.follows.delete({
+        where: { id: follows.id },
+      });
+
+      res.status(201).json({ message: "You have successfully unfollowed" });
+    } catch (error) {
+      console.error("Error while unfollowing", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
 };
 
